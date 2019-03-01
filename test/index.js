dc51e7c382a438b300bcfceb1bca313cb8df808c
Sergey Sova <mail@sergeysova.com>
2019-03-01 23:22:24 +0300

    refactor: tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import test from 'ava'
-import { createSymbiote } from 'redux-symbiote'
+import test from "ava"
+import { createSymbiote } from "redux-symbiote"
 import {
   createFetching,
   fetchStatus,
@@ -9,40 +9,39 @@ import {
   isFailed,
   isInitial,
   isLoading,
-} from '../src/index'
+} from "../src/index"
 
-
-test('fetching status different', (t) => {
+test("fetching status different", (t) => {
   const values = Object.values(initialFetching)
   const unique = [...new Set(values)]
 
   t.is(values.length, unique.length)
 })
 
-test('isReady should be true', (t) => {
-  t.is(isFailed(fetchStatus.ready), false)
-  t.is(isReady(fetchStatus.ready), true)
+test("isInitial should return true only for .initial", (t) => {
+  t.is(isInitial(fetchStatus.initial), true)
+  t.is(isInitial(fetchStatus.loading), false)
   t.is(isInitial(fetchStatus.ready), false)
-  t.is(isLoading(fetchStatus.ready), false)
+  t.is(isInitial(fetchStatus.failed), false)
 })
 
-test('isFailed should be true', (t) => {
-  t.is(isFailed(fetchStatus.failed), true)
-  t.is(isReady(fetchStatus.failed), false)
-  t.is(isInitial(fetchStatus.failed), false)
+test("isLoading should return true only for .loading", (t) => {
+  t.is(isLoading(fetchStatus.initial), false)
+  t.is(isLoading(fetchStatus.loading), true)
+  t.is(isLoading(fetchStatus.ready), false)
   t.is(isLoading(fetchStatus.failed), false)
 })
 
-test('isInitial should be true', (t) => {
-  t.is(isFailed(fetchStatus.initial), false)
+test("isReady should return true only for .ready", (t) => {
   t.is(isReady(fetchStatus.initial), false)
-  t.is(isInitial(fetchStatus.initial), true)
-  t.is(isLoading(fetchStatus.initial), false)
+  t.is(isReady(fetchStatus.loading), false)
+  t.is(isReady(fetchStatus.ready), true)
+  t.is(isReady(fetchStatus.failed), false)
 })
 
-test('isLoading should be true', (t) => {
+test("isFailed should return true only for .failed", (t) => {
+  t.is(isFailed(fetchStatus.initial), false)
   t.is(isFailed(fetchStatus.loading), false)
-  t.is(isReady(fetchStatus.loading), false)
-  t.is(isInitial(fetchStatus.loading), false)
-  t.is(isLoading(fetchStatus.loading), true)
+  t.is(isFailed(fetchStatus.ready), false)
+  t.is(isFailed(fetchStatus.failed), true)
 })
